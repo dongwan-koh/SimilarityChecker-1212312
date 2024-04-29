c3a6222f7ada64be3cdc91ef5e3a82f5173e1826
dw.koh <dw0402.koh@samsung.com>
2024-04-29 15:16:03 +0900

    1번 검사 - 글자 수

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,5 +1,30 @@
 public class SimilarityChecker {
-    public void checkLength(Object o, Object o1) {
-        throw new IllegalArgumentException();
+
+    public static int MAX_LENGTH_POINT = 60;
+    public static int MIN_LENGTH_POINT = 0;
+
+    public int returnLengthScore(String input1, String input2) {
+        if(input1 == null || input2 == null)
+            throw new IllegalArgumentException();
+
+        if (isSameLength(input1, input2))
+            return MAX_LENGTH_POINT;
+        if(isTwiceDiff(input1, input2))
+            return MIN_LENGTH_POINT;
+
+        return getPartialPoint(Integer.max(input1.length(), input2.length()), Integer.min(input1.length(), input2.length()));
+    }
+
+    private int getPartialPoint(int maxLen, int minLen) {
+        double gap = (double) (maxLen - minLen) /minLen;
+        return (int)((1-gap) * 60);
+    }
+
+    private boolean isTwiceDiff(String input1, String input2) {
+        return (input1.length() >= input2.length() * 2) || (input2.length() >= input1.length() * 2);
+    }
+
+    private boolean isSameLength(String input1, String input2) {
+        return input1.length() == input2.length();
     }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SimilarityCheckerTest {
@@ -14,11 +15,30 @@ public class SimilarityCheckerTest {
     @Test
     public void throwIllegalArgumentExceptionInvalidInput() {
         assertIllegalArgument(null, null);
+        assertIllegalArgument("ABC", null);
+        assertIllegalArgument(null, "BCD");
+    }
+
+    @Test
+    public void returnCompareDoubleLength() {
+        assertEquals(0, checker.returnLengthScore("A", "BB"));
+    }
+    @Test
+    public void returnCompareSameLength() {
+        assertEquals(60, checker.returnLengthScore("ABC","DSA"));
+    }
+    @Test
+    public void returnComparePartialScore1() {
+        assertEquals(20, checker.returnLengthScore("AAABB","BAA"));
+    }
+    @Test
+    public void returnComparePartialScore2() {
+        assertEquals(30, checker.returnLengthScore("AA","AAE"));
     }
 
     private void assertIllegalArgument(String input1, String input2) {
         try {
-            checker.checkLength(input1, input2);
+            checker.returnLengthScore(input1, input2);
             fail();
         }
         catch (IllegalArgumentException e) {
