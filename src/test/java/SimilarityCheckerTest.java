57f3c8bb4f4af0e242cdfa14a9221be03106dacc
dw.koh <dw0402.koh@samsung.com>
2024-04-29 14:26:38 +0900

    [refactoring] 1 Cycle

--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,15 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SimilarityCheckerTest {
+    SimilarityChecker checker;
+
+    @BeforeEach
+    public void setUp() {
+        checker = new SimilarityChecker();
+    }
+
     @Test
     public void throwIllegalArgumentExceptionInvalidInput() {
-        SimilarityChecker checker = new SimilarityChecker();
-        String input1 = null;
-        String input2 = null;
+        assertIllegalArgument(null, null);
+    }
+
+    private void assertIllegalArgument(String input1, String input2) {
         try {
-            checker.checkLength(null, null);
+            checker.checkLength(input1, input2);
             fail();
         }
         catch (IllegalArgumentException e) {
