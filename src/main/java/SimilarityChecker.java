7983e83c4a9b397dc4f8ce1a870f2b60ac8d0046
dw.koh <dw0402.koh@samsung.com>
2024-04-29 14:24:25 +0900

    [feature] 1 Cycle

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,5 @@
 public class SimilarityChecker {
+    public void checkLength(Object o, Object o1) {
+        throw new IllegalArgumentException();
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class SimilarityCheckerTest {
+    @Test
+    public void throwIllegalArgumentExceptionInvalidInput() {
+        SimilarityChecker checker = new SimilarityChecker();
+        String input1 = null;
+        String input2 = null;
+        try {
+            checker.checkLength(null, null);
+            fail();
+        }
+        catch (IllegalArgumentException e) {
+
+        }
+    }
 }
